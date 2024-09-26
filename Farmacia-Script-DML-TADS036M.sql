f9dad4ccb699301c72ec94c8c6c4792e85474993
Danilo Farias <danilo.silva@nutes.ufpe.br>
2024-09-26 09:22:36 -0300

    Aula DML

--- Farmacia-Script-DML-TADS036M.sql
+++ Farmacia-Script-DML-TADS036M.sql
@@ -1,16 +1,16 @@
 -- Comandos SQL: DML Insert
 
-insert into cliente (cpf, nome, sexo, email, telefone, dataNasc)
+insert into cliente (cpf, nome, sexo, email, telefone, dataNasc, pontuacao)
 	value ("123.321.456-00", "Maria Gabriella da Silva Calado", 
-		'F', "mgsc@pe.senac.br", "81998975656", "2006-06-18");
+		'F', "mgsc@pe.senac.br", "81998975656", "2006-06-18", 100);
         
-insert into cliente (cpf, nome, sexo, email, telefone, dataNasc)
+insert into cliente (cpf, nome, sexo, email, telefone, dataNasc, pontuacao)
 	values ("333.321.456-00", "Maria Lívia Soares da Silva", 
-		'F', "mlss@pe.senac.br", "81977775656", "2003-02-10"),
+		'F', "mlss@pe.senac.br", "81977775656", "2003-02-10", 150),
         ("222.321.456-00", "João Italo Deodato Santos", 
-		'M', "jids@pe.senac.br", "81999885656", "2004-06-25"),
+		'M', "jids@pe.senac.br", "81999885656", "2004-06-25", 80),
         ("111.321.456-00", "Hadassa Gomes dos Santos", 
-		'F', "hgs@pe.senac.br", "81991175655", "2001-04-02");
+		'F', "hgs@pe.senac.br", "81991175655", "2001-04-02", 30);
         
  insert into enderecocli (cliente_cpf, uf, cidade, bairro, rua, 
 	numero, comp, cep)
