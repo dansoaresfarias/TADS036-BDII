3464a854f793a7aaed2486bd1ffdb68d3ffd242e
Danilo Farias <danilo.silva@nutes.ufpe.br>
2024-10-10 09:41:30 -0300

    Aula de DQL

--- Farmacia-Script-DQL-TADS036M.sql
+++ Farmacia-Script-DQL-TADS036M.sql
@@ -9,3 +9,106 @@ select cpf as "CPF", nome "Funcionário", sexo "Gênero", dataNasc "Data de Nasc
 	estadoCivil "Estado Civil", email "E-mail", ch "Carga-horária", salario "Salário", 
     comissao "Comissão", dataAdm "Data de Admissão" 
 	from funcionario;
+    
+select cpf as "CPF", nome "Funcionário", sexo "Gênero", dataNasc "Data de Nascimento", 
+	estadoCivil "Estado Civil", email "E-mail", ch "Carga-horária", salario "Salário", 
+    comissao "Comissão", dataAdm "Data de Admissão" 
+	from funcionario
+		order by nome desc;
+        
+select cpf as "CPF", nome "Funcionário", sexo "Gênero", dataNasc "Data de Nascimento", 
+	estadoCivil "Estado Civil", email "E-mail", ch "Carga-horária", salario "Salário", 
+    comissao "Comissão", dataAdm "Data de Admissão" 
+	from funcionario
+		where sexo = 'F'
+			order by nome;        
+        
+select cpf as "CPF", nome "Funcionário", sexo "Gênero", dataNasc "Data de Nascimento", 
+	estadoCivil "Estado Civil", email "E-mail", ch "Carga-horária", salario "Salário", 
+    comissao "Comissão", dataAdm "Data de Admissão" 
+	from funcionario
+		where salario >= 3000 and
+			sexo = 'M'
+			order by nome;        
+        
+select cpf as "CPF", nome "Funcionário", sexo "Gênero", dataNasc "Data de Nascimento", 
+	estadoCivil "Estado Civil", email "E-mail", ch "Carga-horária", salario "Salário", 
+    comissao "Comissão", dataAdm "Data de Admissão" 
+	from funcionario
+		where dataAdm < '2020-03-04'
+			order by nome;        
+        
+select cpf as "CPF", nome "Funcionário", sexo "Gênero", dataNasc "Data de Nascimento", 
+	estadoCivil "Estado Civil", email "E-mail", ch "Carga-horária", salario "Salário", 
+    comissao "Comissão", dataAdm "Data de Admissão" 
+	from funcionario
+		where dataAdm >= '2020-06-01' and
+			dataAdm <= '2022-06-30'
+			order by nome;          
+        
+select cpf as "CPF", nome "Funcionário", sexo "Gênero", dataNasc "Data de Nascimento", 
+	estadoCivil "Estado Civil", email "E-mail", ch "Carga-horária", salario "Salário", 
+    comissao "Comissão", dataAdm "Data de Admissão" 
+	from funcionario
+		where dataAdm between '2020-06-01' and '2022-06-30'
+			order by nome;        
+        
+select cpf as "CPF", nome "Funcionário", sexo "Gênero", dataNasc "Data de Nascimento", 
+	estadoCivil "Estado Civil", email "E-mail", ch "Carga-horária", salario "Salário", 
+    comissao "Comissão", dataAdm "Data de Admissão" 
+	from funcionario
+		where salario between 4000 and 5000
+			order by nome;          
+
+select cpf as "CPF", nome "Funcionário", sexo "Gênero", dataNasc "Data de Nascimento", 
+	estadoCivil "Estado Civil", email "E-mail", ch "Carga-horária", salario "Salário", 
+    comissao "Comissão", dataAdm "Data de Admissão" 
+	from funcionario
+		where nome like "%Costa"
+			order by nome;    
+            
+            
+select cpf as "CPF", nome "Funcionário", sexo "Gênero", dataNasc "Data de Nascimento", 
+	estadoCivil "Estado Civil", email "E-mail", ch "Carga-horária", salario "Salário", 
+    comissao "Comissão", dataAdm "Data de Admissão" 
+	from funcionario
+		where nome like "Ana%"
+			order by nome;  
+            
+select cpf as "CPF", nome "Funcionário", sexo "Gênero", dataNasc "Data de Nascimento", 
+	estadoCivil "Estado Civil", email "E-mail", ch "Carga-horária", salario "Salário", 
+    comissao "Comissão", dataAdm "Data de Admissão" 
+	from funcionario
+		where nome like "%Ana%"
+			order by nome;
+            
+select cpf as "CPF", nome "Funcionário", sexo "Gênero", dataNasc "Data de Nascimento", 
+	estadoCivil "Estado Civil", email "E-mail", ch "Carga-horária", salario "Salário", 
+    comissao "Comissão", dataAdm "Data de Admissão" 
+	from funcionario
+		where nome like "%Costa" or
+			nome like "%Silva" or
+            nome like "%Oliveira"
+			order by nome;
+            
+select cpf as "CPF", upper(nome) "Funcionário", sexo "Gênero", 
+	date_format(dataNasc, '%d/%m/%Y') "Data de Nascimento", 
+	estadoCivil "Estado Civil", email "E-mail", concat(ch, 'h') "Carga-horária", 
+    concat('R$ ', format(salario, 2, 'de_DE')) "Salário", 
+    concat('R$ ', format(comissao, 2, 'de_DE')) "Comissão", 
+    concat('R$ ', format(salario + comissao, 2, 'de_DE')) "Salário Final",
+    date_format(dataAdm, '%H:%i - %d/%m/%Y') "Data de Admissão" 
+	from funcionario
+		order by salario+comissao desc;
+
+select cpf as "CPF", upper(nome) "Funcionário", sexo "Gênero", 
+	date_format(dataNasc, '%d/%m/%Y') "Data de Nascimento", 
+	estadoCivil "Estado Civil", email "E-mail", concat(ch, 'h') "Carga-horária", 
+    concat('R$ ', format(salario, 2, 'de_DE')) "Salário", 
+    concat('R$ ', format(comissao, 2, 'de_DE')) "Comissão", 
+    concat('R$ ', format(salario + comissao, 2, 'de_DE')) "Salário Final",
+    date_format(dataAdm, '%H:%i - %d/%m/%Y') "Data de Admissão" 
+	from funcionario
+		order by nome;
+
+
