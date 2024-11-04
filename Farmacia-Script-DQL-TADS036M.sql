1afd3f327a23a2d3c8adbbef23a06f3b34c5bd07
Danilo Farias <danilo.silva@nutes.ufpe.br>
2024-11-04 11:23:34 -0300

    Update Farmacia-Script-DQL-TADS036M.sql

--- Farmacia-Script-DQL-TADS036M.sql
+++ Farmacia-Script-DQL-TADS036M.sql
@@ -456,16 +456,98 @@ select func.cpf "CPF", upper(func.nome) "Funcionário",
 	from funcionario func
 		order by func.nome;
 
+-- Questões do petshop
+-- Relatório 1 - Lista dos empregados admitidos entre 2019-01-01 e 2022-03-31, 
+-- trazendo as colunas (Nome Empregado, CPF Empregado, Data Admissão,  Salário, 
+-- Departamento, Número de Telefone), ordenado por data de admissão decrescente;
+select upper(emp.nome) "Nome Empregado", emp.cpf "CPF Empregado", 
+	date_format(emp.dataAdm, '%H:%i - %d/%m/%Y') "Data Admissão",  
+	concat("R$ ", format(emp.salario, 2, 'de_DE'))"Salário", 
+    dep.nome "Departamento", tel.numero "Número de Telefone" 
+	from empregado emp
+		inner join departamento dep on dep.idDepartamento = emp.Departamento_idDepartamento
+		left join telefone tel on tel.empregado_cpf = emp.cpf
+			where emp.dataAdm between '2019-01-01' and '2023-03-31'
+				order by emp.dataAdm desc;
 
 
+-- Relatório 2 - Lista dos empregados que ganham menos que a média salarial dos funcionários 
+-- do Petshop, trazendo as colunas (Nome Empregado, CPF Empregado, Data Admissão,  
+-- Salário, Departamento, Número de Telefone), ordenado por nome do empregado;
+select upper(emp.nome) "Nome Empregado", emp.cpf "CPF Empregado", 
+	date_format(emp.dataAdm, '%H:%i - %d/%m/%Y') "Data Admissão",  
+	concat("R$ ", format(emp.salario, 2, 'de_DE')) "Salário", 
+    dep.nome "Departamento", tel.numero "Número de Telefone" 
+	from empregado emp
+    inner join departamento dep on dep.idDepartamento = emp.Departamento_idDepartamento
+	left join telefone tel on tel.empregado_cpf = emp.cpf
+		where salario <= (select avg(salario) from empregado)
+				order by emp.nome;
 
+-- Relatório 3 - Lista dos departamentos com a quantidade de empregados total por 
+-- cada departamento, trazendo também a média salarial dos funcionários do departamento 
+-- e a média de comissão recebida pelos empregados do departamento, com as colunas 
+-- (Departamento, Quantidade de Empregados, Média Salarial, Média da Comissão), 
+-- ordenado por nome do departamento;
+select dep.nome "Departamento", count(emp.cpf) "Quantidade de Empregados", 
+	concat("R$ ", format(avg(emp.salario), 2, 'de_DE')) "Média Salarial", 
+    concat("R$ ", format(avg(emp.comissao), 2, 'de_DE')) "Média da Comissão"
+	from departamento dep
+		left join empregado emp on emp.Departamento_idDepartamento = dep.idDepartamento
+		group by dep.idDepartamento
+			order by dep.nome;
 
+-- Relatório 4 - Lista dos empregados com a quantidade total de vendas já realiza 
+-- por cada Empregado, além da soma do valor total das vendas do empregado e a soma 
+-- de suas comissões, trazendo as colunas (Nome Empregado, CPF Empregado, Sexo, Salário, 
+-- Quantidade Vendas, Total Valor Vendido, Total Comissão das Vendas), ordenado por 
+-- quantidade total de vendas realizadas;
+select upper(emp.nome) "Nome Empregado", emp.cpf "CPF Empregado", emp.sexo "Gênero",
+	concat("R$ ", format(emp.salario, 2, 'de_DE')) "Salário", 
+    count(vnd.idVenda) "Quantidade Vendas", 
+    concat("R$ ", format(sum(vnd.valor - vnd.desconto), 2, 'de_DE')) "Total Valor Vendido", 
+    concat("R$ ", format(sum(vnd.comissao), 2, 'de_DE')) "Total Comissão das Vendas"
+	from empregado emp
+		left join venda vnd on vnd.Empregado_cpf = emp.cpf
+			group by emp.cpf
+				order by count(vnd.idVenda) desc;
 
+-- Relatório 5 - Lista dos empregados que prestaram Serviço na venda computando a 
+-- quantidade total de vendas realizadas com serviço por cada Empregado, além da soma do 
+-- valor total apurado pelos serviços prestados nas vendas por empregado e a soma de suas 
+-- comissões, trazendo as colunas (Nome Empregado, CPF Empregado, Sexo, Salário, Quantidade 
+-- Vendas com Serviço, Total Valor Vendido com Serviço, Total Comissão das Vendas com Serviço)
+-- , ordenado por quantidade total de vendas realizadas;
+select upper(emp.nome) "Nome Empregado", emp.cpf "CPF Empregado", emp.sexo "Gênero",
+	concat("R$ ", format(emp.salario, 2, 'de_DE')) "Salário", 
+    count(vnd.idVenda) "Quantidade de Vendas com Serviço", 
+    concat("R$ ", format(sum((isv.valor - isv.desconto) * isv.quantidade), 2, 'de_DE')) 
+		"Total Valor Vendido com Serviço",
+    concat("R$ ", format(sum(vnd.comissao), 2, 'de_DE')) 
+		"Total Comissão das Vendas com Serviço"
+	 from empregado emp
+		inner join itensservico isv on isv.Empregado_cpf = emp.cpf
+        inner join servico srv on srv.idServico = isv.Servico_idServico
+        inner join venda vnd on vnd.idVenda = isv.Venda_idVenda
+			group by emp.cpf
+				order by count(vnd.idVenda) desc;
 
-
-
-
-
-
-
-            
+-- Relatório 6 - Lista dos serviços já realizados por um Pet, trazendo as colunas 
+-- (Nome do Pet, Data do Serviço, Nome do Serviço, Quantidade, Valor, 
+-- Empregado que realizou o Serviço), ordenado por data do serviço da mais recente a 
+-- mais antiga;  
+select p.nome "Nome do Pet", vnd.`data` "Data do Serviço", srv.nome "Nome do Serviço", 
+	isv.quantidade "Quantidade", 
+    concat("R$ ", format(isv.valor, 2, 'de_DE')) "Valor", 
+	emp.nome "Empregado que realizou o Serviço"
+	 from pet p 
+		inner join itensservico isv on isv.pet_idpet = p.idpet
+        inner join servico srv on srv.idServico = isv.Servico_idServico
+        inner join venda vnd on vnd.idVenda = isv.Venda_idVenda
+		inner join empregado emp on emp.cpf = isv.Empregado_cpf
+			order by vnd.`data` desc;
+  
+  
+  
+  
+  
